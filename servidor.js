3fe0a4ce9045b490dbc964eaee2afc49bae07f31
Fernando D'abenigno <dabenignofernando32@gmail.com>
2022-09-22 22:31:05 -0300

    desafio3

---
+++ servidor.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const Contenedor = require('./Container')
+const PORT = 8080
+const productos= new Contenedor ('productos.txt')
+app.get('/', (req, res)=>{
+    res.send('Hola Estoy en Coder ahora')
+    console.log(productos.read())
+})
+app.get('/productos', (req, res)=>{
+    res.json(productos.getAll())
+    console.log(productos.getAll());
+})
+
+app.get('/productosRandon', (req, res) => {
+    res.json(productos.getRandomProduct())
+
+
+
+})
+app.listen(PORT, ()=>{
+    console.log(`Servidor recibiendo en puerto ${PORT}`);
+})
